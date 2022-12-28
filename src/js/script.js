4f111e140612040be2507c302fa837103d6b72db
Victória Almeida <mariavictoriafalmeida@gmail.com>
2022-12-28 19:19:13 -0300

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,5 @@ function somar(){
     var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
     var s = n1 + n2
-    res.innerHTML = `A soma entre <strong>${n1}</strong> e <strong>${n2}</strong> é igual a <span>${s}</span>`
+    res.innerHTML = `a soma entre <strong>${n1}</strong> e <strong>${n2}</strong> é igual a <span>${s}</span>`
 }
